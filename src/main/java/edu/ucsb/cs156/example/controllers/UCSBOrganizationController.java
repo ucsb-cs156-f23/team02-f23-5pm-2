70dd4a74c9826b2471382c74d60c5f070b49b32a
Adam <adamnobunaga@gmail.com>
2023-10-29 17:50:36 -0700

    modified controller

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Tag(name = "UCSBOrganizations")
+@Tag(name = "UCSBOrganization")
 @RequestMapping("/api/ucsborganization")
 @RestController
 @Slf4j
